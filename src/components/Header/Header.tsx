1276ce3e54c13863b1b23e220729e9b9ec06fde5
Lincoln <lincoln.choy1997@gmail.com>
2020-12-10 23:14:43 +1300

    Added header routing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
-import React  from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Header extends React.Component<any, any> {
     render() {
         return (
             <div className="app-header">
-                <div className="link-button">Home</div>
-                <div className="link-button">How to practice</div>
-                <div className="link-button">Test</div>
+                <Link to="/"><div className="link-button">Home</div></Link>
+                <Link to="/how"><div className="link-button">How to practice</div></Link>
+                <Link to="/test"><div className="link-button">Test</div></Link>
             </div>
         )
     }
