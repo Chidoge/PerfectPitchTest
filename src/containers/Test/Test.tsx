2222fc50d1c4df6b6bf463b3981ff7aa338048d8
Henry <hen.2005@hotmail.com>
2020-12-11 12:37:28 +1300

    In progress, Added options for choices, added template for pitch test page

--- src/containers/Test/Test.tsx
+++ src/containers/Test/Test.tsx
@@ -3,8 +3,31 @@ import React from 'react'
 export default class Test extends React.Component<any, any> {
     render() {
         return (
-            <div className="">
-
+            <div className="test-page">
+                <div className="test-container">
+                    <h1>Perfect Pitch Test</h1>
+                    <div>Score: 0 / 0 correct</div>
+                    <button className="button">Hear Again</button>
+                    <h1>Choices</h1>
+                    <div>
+                        <button className="choices">C</button>
+                        <button className="choices">D</button>
+                        <button className="choices">E</button>
+                    </div>
+                    <button className="button">End Quiz</button>
+                </div>
+                <div className="options-container">
+                    <h1>Options</h1>
+                    <p>Select the instrument which you would like played</p>
+                    <select name="instruments">
+                        
+                    </select>
+                    <p>Select the notes on which you would like to be tested</p>
+                    <button className="choices">C</button>
+                    <button className="choices">D</button>
+                    <button className="choices">E</button>
+                </div>
+                
             </div>
         )
     }
---
+++ src/containers/Test/options.ts
@@ -0,0 +1,5 @@
+var options = {
+    notes: [ "A", "A♯/B♭", "B", "C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭"]
+}
+
+export default options;
