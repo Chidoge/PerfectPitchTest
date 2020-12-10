2e757159db1f5fb92e2064a2a3c592fc53e580c6
Lincoln <lincoln.choy1997@gmail.com>
2020-12-10 19:08:18 +1300

    Added react router

---
+++ src/components/StyledComponents/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Button extends React.Component<any, any> {
+    render() {
+        return (
+            <button>Test</button>
+        )
+    }
+}
---
+++ src/components/StyledComponents/Input/Input.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Input extends React.Component<any, any> {
+    render() {
+        return (
+            <input />
+        )
+    }
+}
