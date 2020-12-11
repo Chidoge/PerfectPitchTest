879bf2308c850ac29370c05139431566714f21bf
Henry <hen.2005@hotmail.com>
2020-12-11 17:06:04 +1300

    Test page done-ish.

--- src/containers/Test/Test.tsx
+++ src/containers/Test/Test.tsx
@@ -8,24 +8,18 @@ export default class Test extends React.Component<any, any> {
         this.state = {
             selectedNotes: ["C", "D"],
             selectedInstrument: "Piano",
-            notes: [],
             instrumentChoices: []
         }
     }
 
     componentDidMount() {
-        let notes = [];
         let instruments = [];
-
-        for (let note of options.notes) {
-            notes.push(<Button onClick={() => this.selectNote(note)}>{note}</Button>);
-        }
 
         for (let instrument of options.instruments) {
             instruments.push(<option value={instrument.toLowerCase()}>{instrument}</option>);
         }
 
-        this.setState({notes, instrumentChoices: instruments});
+        this.setState({instrumentChoices: instruments});
     }
 
     selectNote(note: string) {
@@ -43,36 +37,56 @@ export default class Test extends React.Component<any, any> {
     }
 
     render() {
-        const { instrumentChoices, notes } = this.state
+        const { instrumentChoices } = this.state
 
         let selectedNotes = [];
+        let notes = [];
         const ordered = this.state.selectedNotes.sort()
 
         for (let selectedNote of ordered) {
+            selectedNotes.push(<Button>{selectedNote}</Button>);
+        }
+
+        for (let i = 0; i < options.notes.length; i++) {
+            let note = options.notes[i];
+            let nextNote = options.notes[i+1]
             
-            selectedNotes.push(<button className="choices">{selectedNote}</button>);
+            if (nextNote.charAt(0) === note) {
+                notes.push(
+                    <div>
+                        <Button isHighlighted={ordered.includes(note)} onClick={() => this.selectNote(note)}>{note}</Button>
+                        <Button isHighlighted={ordered.includes(nextNote)} onClick={() => this.selectNote(nextNote)}>{nextNote}</Button>
+                    </div>
+                )
+                i++
+            }
+            else {
+                notes.push(<Button isHighlighted={ordered.includes(note)} onClick={() => this.selectNote(note)}>{note}</Button>);
+            }
         }
 
         return (
-            <div className="test-page">
-                <div className="test-container">
+            <div className="test-page container-flex-column-center">
+                <div className="test-container container-flex-column-center">
                     <h1>Perfect Pitch Test</h1>
                     <div>Score: 0 / 0 correct</div>
-                    <button className="button">Hear Again</button>
+                    <Button>Hear Again</Button>
                     <h1>Choices</h1>
                     <div>
                         {selectedNotes}
                     </div>
-                    <button className="button">End Quiz</button>
+                    <Button className="button">End Quiz</Button>
                 </div>
-                <div className="options-container">
+                <div className="options-container container-flex-column-center">
                     <h1>Options</h1>
                     <p>Select the instrument which you would like played</p>
                     <select name="instruments">
                         {instrumentChoices}
                     </select>
                     <p>Select the notes on which you would like to be tested</p>
-                    {notes}
+                    <div className="note-container">
+                        {notes}
+                    </div>
                 </div>
                 
             </div>
