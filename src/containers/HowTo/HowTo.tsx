7c70837f8933ecf2bd057912a7582e1c7d734ffc
Lincoln <lincoln.choy1997@gmail.com>
2020-12-11 15:34:12 +1300

    WIP

---
+++ src/containers/HowTo/HowTo.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class HowTo extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="home-page">
+                <div className="main-section">
+                    <h1><i>Consistency is KEY</i></h1>
+                    <b>Make sure you practice everyday</b>
+                    <p>Even just 5 minutes a day, everyday for a month will make a huge difference.</p>
+                    <p>Even better would be multiple 5 minute sessions sprinkled throughout the day.</p>
+                    <p>Make sure to start with a couple notes and then start adding more notes as you become more confident with recognising a few without reference notes.</p>
+                </div>
+            </div>
+        )
+    }
+}
